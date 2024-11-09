0749b36189ac0e14f70c7071582bdc1d38651ca0
Zack <zack.scholl@gmail.com>
2024-11-09 12:13:34 -0800

    allow multiple pages

--- lib/flashmem.h
+++ lib/flashmem.h
@@ -23,48 +23,66 @@ void DataStruct_unmarshal(DataStruct *data, const uint8_t *buffer) {
   memcpy(data, buffer, sizeof(DataStruct));
 }
 
-void pico_flash_read(uint8_t *buffer, size_t len) {
+void pico_flash_read(uint8_t *buffer, size_t len, size_t offset) {
   const uint8_t *flash_target_contents =
-      (const uint8_t *)(XIP_BASE + FLASH_TARGET_OFFSET);
+      (const uint8_t *)(XIP_BASE + FLASH_TARGET_OFFSET +
+                        (offset * FLASH_PAGE_SIZE));
   for (size_t i = 0; i < len; ++i) {
     buffer[i] = flash_target_contents[i];
   }
 }
 
-void pico_flash_write(uint8_t *buffer) {
+void pico_flash_erase() {
   uint32_t interrupts = save_and_disable_interrupts();
   flash_range_erase(FLASH_TARGET_OFFSET, FLASH_SECTOR_SIZE);
-  flash_range_program(FLASH_TARGET_OFFSET, buffer, FLASH_PAGE_SIZE);
+  restore_interrupts(interrupts);
+}
+
+void pico_flash_write(uint8_t *buffer, size_t offset) {
+  uint32_t interrupts = save_and_disable_interrupts();
+  flash_range_program(FLASH_TARGET_OFFSET + offset * FLASH_PAGE_SIZE, buffer,
+                      FLASH_PAGE_SIZE);
   restore_interrupts(interrupts);
 }
 
 void flash_mem_test() {
   uint8_t flash_data[FLASH_PAGE_SIZE];
-  pico_flash_read(flash_data, FLASH_PAGE_SIZE);
-
-  DataStruct *data = malloc(sizeof(DataStruct));
-  DataStruct_unmarshal(data, flash_data);
-  printf("DataStruct: %d %d %d %d %2.3f\n", data->A, data->B, data->C, data->D,
-         data->E);
-
-  data->A++;
-  data->B++;
-  data->C++;
-  data->D++;
-  data->E += 3.14;
-
-  if (data->A > 122953999) {
-    // reset all to 0
-    data->A = 0;
-    data->B = 0;
-    data->C = 0;
-    data->D = 0;
-    data->E = 0.0;
+  // make an array of 4 DataStructs
+  DataStruct *data[4];
+  for (int i = 0; i < 4; i++) {
+    data[i] = malloc(sizeof(DataStruct));
   }
 
-  // marshal data
-  DataStruct_marshal(data, flash_data);
-  pico_flash_write(flash_data);
+  for (int i = 0; i < 4; i++) {
+    pico_flash_read(flash_data, FLASH_PAGE_SIZE, i);
+    DataStruct_unmarshal(data[i], flash_data + i * sizeof(DataStruct));
+    printf("DataStruct[%d]: %d %d %d %d %2.3f\n", i, data[i]->A, data[i]->B,
+           data[i]->C, data[i]->D, data[i]->E);
+    if (data[i]->A > 122953999 || data[i]->E < -1000) {
+      // reset all to 0
+      data[i]->A = 0;
+      data[i]->B = 0;
+      data[i]->C = 0;
+      data[i]->D = 0;
+      data[i]->E = 0.0;
+    }
+    data[i]->A += 1;
+    data[i]->B += 2;
+    data[i]->C += 3;
+    data[i]->D += 4;
+    data[i]->E += 0.1;
+  }
 
-  free(data);
+  // erase sector
+  pico_flash_erase();
+
+  // write data
+  for (int i = 0; i < 4; i++) {
+    DataStruct_marshal(data[i], flash_data + i * sizeof(DataStruct));
+    pico_flash_write(flash_data, i);
+  }
+
+  for (int i = 0; i < 4; i++) {
+    free(data[i]);
+  }
 }
