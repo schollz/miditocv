e0ec21a1304d40a41a24a2a2b97e5a106e21a72f
Zack <zack.scholl@gmail.com>
2024-11-09 11:59:55 -0800

    simplify

--- lib/flashmem.h
+++ lib/flashmem.h
@@ -12,39 +12,15 @@ typedef struct {
   int32_t B;
   int32_t C;
   int32_t D;
+  float E;
 } DataStruct;
 
-void DataStruct_marshal(DataStruct *data, uint8_t *buffer) {
-  buffer[0] = (data->A >> 24) & 0xFF;
-  buffer[1] = (data->A >> 16) & 0xFF;
-  buffer[2] = (data->A >> 8) & 0xFF;
-  buffer[3] = data->A & 0xFF;
-
-  buffer[4] = (data->B >> 24) & 0xFF;
-  buffer[5] = (data->B >> 16) & 0xFF;
-  buffer[6] = (data->B >> 8) & 0xFF;
-  buffer[7] = data->B & 0xFF;
-
-  buffer[8] = (data->C >> 24) & 0xFF;
-  buffer[9] = (data->C >> 16) & 0xFF;
-  buffer[10] = (data->C >> 8) & 0xFF;
-  buffer[11] = data->C & 0xFF;
-
-  buffer[12] = (data->D >> 24) & 0xFF;
-  buffer[13] = (data->D >> 16) & 0xFF;
-  buffer[14] = (data->D >> 8) & 0xFF;
-  buffer[15] = data->D & 0xFF;
+void DataStruct_marshal(const DataStruct *data, uint8_t *buffer) {
+  memcpy(buffer, data, sizeof(DataStruct));
 }
 
-void DataStruct_unmarshal(DataStruct *data, uint8_t *buffer) {
-  data->A =
-      (buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | buffer[3];
-  data->B =
-      (buffer[4] << 24) | (buffer[5] << 16) | (buffer[6] << 8) | buffer[7];
-  data->C =
-      (buffer[8] << 24) | (buffer[9] << 16) | (buffer[10] << 8) | buffer[11];
-  data->D =
-      (buffer[12] << 24) | (buffer[13] << 16) | (buffer[14] << 8) | buffer[15];
+void DataStruct_unmarshal(DataStruct *data, const uint8_t *buffer) {
+  memcpy(data, buffer, sizeof(DataStruct));
 }
 
 void pico_flash_read(uint8_t *buffer, size_t len) {
@@ -68,12 +44,14 @@ void flash_mem_test() {
 
   DataStruct *data = malloc(sizeof(DataStruct));
   DataStruct_unmarshal(data, flash_data);
-  printf("DataStruct: %d %d %d %d\n", data->A, data->B, data->C, data->D);
+  printf("DataStruct: %d %d %d %d %2.3f\n", data->A, data->B, data->C, data->D,
+         data->E);
 
   data->A++;
   data->B++;
   data->C++;
   data->D++;
+  data->E += 3.14;
 
   if (data->A > 122953999) {
     // reset all to 0
@@ -81,6 +59,7 @@ void flash_mem_test() {
     data->B = 0;
     data->C = 0;
     data->D = 0;
+    data->E = 0.0;
   }
 
   // marshal data
