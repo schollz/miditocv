badd85612c118fd8fe4b52862844b31d251add93
Zack Scholl <zack.scholl@gmail.com>
2024-11-26 21:35:30 -0800

    add lua

--- main.c
+++ main.c
@@ -54,6 +54,9 @@ static const uint32_t PIN_DCDC_PSM_CTRL = 23;
 #include "my_debug.h"
 #include "sd_card.h"
 //
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 //
 #include "lib/WS2812.h"
 #include "lib/adsr.h"
@@ -471,6 +474,46 @@ int main() {
 
   uint32_t ct_last = ct;
 
+  sleep_ms(3000);
+
+  lua_State *L = luaL_newstate();  // Create a new Lua state
+  luaL_openlibs(L);                // Open standard libraries
+
+  // Define the Lua script directly as a string
+  const char *lua_code =
+      "function random_number()\n"
+      "  return math.random(1, 10)+100.123\n"
+      "end";
+
+  // Run the Lua script to define the function
+  if (luaL_dostring(L, lua_code) != LUA_OK) {
+    printf("Error: %s\n", lua_tostring(L, -1));
+    lua_close(L);
+    return 1;
+  }
+
+  // Set up random number generator seed
+  luaL_dostring(L, "math.randomseed(os.time())");
+
+  // Call the Lua function and get the result
+  for (int i = 0; i < 5; i++) {
+    lua_getglobal(L, "random_number");      // Get the function from Lua
+    if (lua_pcall(L, 0, 1, 0) != LUA_OK) {  // Call the function
+      printf("Error: %s\n", lua_tostring(L, -1));
+      lua_close(L);
+      return 1;
+    }
+
+    double val = lua_tonumber(L, -1);  // Get the return value as a float
+    lua_pop(L, 1);                     // Pop the result from the stack
+    printf("random float: %.2f\n", val);
+
+    sleep_ms(1000);  // Sleep for 1 second (use sleep(1) instead of sleep_ms)
+  }
+
+  lua_close(L);  // Clean up the Lua state
+
+  sleep_ms(1000);
   while (true) {
 #ifdef INCLUDE_MIDI
     tud_task();
