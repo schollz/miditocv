788eca1dc75701695cbac110d79d6ff906fcf1c2
Zack Scholl <zack.scholl@gmail.com>
2024-11-28 02:30:21 -0800

    coloea

--- main.c
+++ main.c
@@ -199,7 +199,9 @@ void timer_callback_ws2812(bool on, int user_data) {
     }
   }
   for (uint8_t i = 8; i < 16; i++) {
-    WS2812_fill(&ws2812, i, 255, 255, 255);
+    Config *config = &yocto.config[yocto.i][i - 8];
+    WS2812_fill(&ws2812, i, const_colors[config->mode][0],
+                const_colors[config->mode][1], const_colors[config->mode][2]);
   }
   WS2812_show(&ws2812);
 }
@@ -468,46 +470,6 @@ int main() {
   SimpleTimer_start(&pool_timer[11], ct);
 
   uint32_t ct_last = ct;
-
-  // sleep_ms(3000);
-
-  // lua_State *L = luaL_newstate();  // Create a new Lua state
-  // luaL_openlibs(L);                // Open standard libraries
-
-  // // Define the Lua script directly as a string
-  // const char *lua_code =
-  //     "function random_number()\n"
-  //     "  return math.random(1, 10)+100.123\n"
-  //     "end";
-
-  // // Run the Lua script to define the function
-  // if (luaL_dostring(L, lua_code) != LUA_OK) {
-  //   printf("Error: %s\n", lua_tostring(L, -1));
-  //   lua_close(L);
-  //   return 1;
-  // }
-
-  // // Set up random number generator seed
-  // luaL_dostring(L, "math.randomseed(os.time())");
-
-  // // Call the Lua function and get the result
-  // for (int i = 0; i < 5; i++) {
-  //   lua_getglobal(L, "random_number");      // Get the function from Lua
-  //   if (lua_pcall(L, 0, 1, 0) != LUA_OK) {  // Call the function
-  //     printf("Error: %s\n", lua_tostring(L, -1));
-  //     lua_close(L);
-  //     return 1;
-  //   }
-
-  //   double val = lua_tonumber(L, -1);  // Get the return value as a float
-  //   lua_pop(L, 1);                     // Pop the result from the stack
-  //   printf("random float: %.2f\n", val);
-
-  //   sleep_ms(1000);  // Sleep for 1 second (use sleep(1) instead of sleep_ms)
-  // }
-
-  // lua_close(L);  // Clean up the Lua state
-  // sleep_ms(1000);
 
   printf("Starting main loop\n");
 
