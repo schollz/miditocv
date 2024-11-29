fbf2ec03e4e63fbcfd0bb7bcf4b2ecbf44ade608
Zack <zack.scholl@gmail.com>
2024-11-29 09:43:10 -0800

    luavm working

--- lib/luavm.h
+++ lib/luavm.h
@@ -13,93 +13,127 @@ int _unlink(const char *pathname) {
 }
 
 //
+#ifndef __linux__
 #include <lauxlib.h>
 #include <lua.h>
 #include <lualib.h>
+#endif
 //
 #include "script.h"
 #include "sequins.h"
 
 lua_State *L;
-int runlua() {
-  if (L == NULL) {
-    L = luaL_newstate();  // Create a new Lua state
-    luaL_openlibs(L);     // Open standard libraries
+int luaInit() {
+  if (L != NULL) {
+    return 0;
+  }
+  L = luaL_newstate();  // Create a new Lua state
+  luaL_openlibs(L);     // Open standard libraries
 
-    // Load Lua script from embedded string
-    if (luaL_loadbuffer(L, (const char *)lib_script_lua, lib_script_lua_len,
-                        "embedded_script") != LUA_OK) {
-      printf("Error loading Lua script: %s\n", lua_tostring(L, -1));
-      lua_close(L);
-      return 1;
-    }
-
-    // Execute the loaded Lua script
-    if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
-      printf("Error executing Lua script: %s\n", lua_tostring(L, -1));
-      lua_close(L);
-      return 1;
-    }
-
-    // Load Lua script from embedded string
-    if (luaL_loadbuffer(L, (const char *)lib_sequins_lua, lib_sequins_lua_len,
-                        "embedded_script") != LUA_OK) {
-      printf("Error loading Lua script: %s\n", lua_tostring(L, -1));
-      lua_close(L);
-      return 1;
-    }
-
-    // Execute the loaded Lua script
-    if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
-      printf("Error executing Lua script: %s\n", lua_tostring(L, -1));
-      lua_close(L);
-      return 1;
-    }
-
-    // Define the new Lua function addone()
-    const char *addone_code =
-        "function addone()\n"
-        "  local num = random_number()\n"
-        "  return num + 0.1\n"
-        "end\n"
-        "abc = S{1, S{2, 3}, 3}";
-
-    // Execute the new Lua function definition
-    if (luaL_dostring(L, addone_code) != LUA_OK) {
-      printf("Error defining addone function: %s\n", lua_tostring(L, -1));
-      lua_close(L);
-      return 1;
-    }
-
-    // Set up random number generator seed
-    luaL_dostring(L, "math.randomseed(os.time())");
+  // Load Lua script from embedded string
+  if (luaL_loadbuffer(L, (const char *)lib_script_lua, lib_script_lua_len,
+                      "embedded_script") != LUA_OK) {
+    printf("Error loading Lua script: %s\n", lua_tostring(L, -1));
+    lua_close(L);
+    return 1;
   }
 
-  // Call the Lua function and get the result
-  for (int i = 0; i < 10; i++) {
-    lua_getglobal(L, "abc");                // Get the function from Lua
-    if (lua_pcall(L, 0, 1, 0) != LUA_OK) {  // Call the function
-      printf("Error: %s\n", lua_tostring(L, -1));
-      lua_close(L);
-      return 1;
-    }
-
-    double val = lua_tonumber(L, -1);  // Get the return value as a double
-    lua_pop(L, 1);                     // Pop the result from the stack
-    printf("random float: %.2f\n", val);
-
-    if (i < 3) {
-      lua_getglobal(L, "abc");       // Push 'abc' table onto the stack
-      lua_getfield(L, -1, "reset");  // Get 'reset' method from 'abc'
-      lua_pushvalue(L, -2);  // Push 'abc' table as the first argument (self)
-      if (lua_pcall(L, 1, 0, 0) != LUA_OK) {  // Call 'reset' with 1 argument
-        printf("Error calling abc:reset: %s\n", lua_tostring(L, -1));
-        lua_pop(L, 1);  // Pop error message
-      }
-      lua_pop(L, 1);  // Pop 'abc' table from stack
-    }
+  // Execute the loaded Lua script
+  if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+    printf("Error executing Lua script: %s\n", lua_tostring(L, -1));
+    lua_close(L);
+    return 1;
   }
 
-  // lua_close(L);  // Close the Lua state
+  // Load Lua script from embedded string
+  if (luaL_loadbuffer(L, (const char *)lib_sequins_lua, lib_sequins_lua_len,
+                      "embedded_script") != LUA_OK) {
+    printf("Error loading Lua script: %s\n", lua_tostring(L, -1));
+    lua_close(L);
+    return 1;
+  }
+
+  // Execute the loaded Lua script
+  if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+    printf("Error executing Lua script: %s\n", lua_tostring(L, -1));
+    lua_close(L);
+    return 1;
+  }
+  return 0;
+}
+
+/**
+function update_env(i, code)
+    envs[i] = new_env(code)
+end
+**/
+int luaUpdateEnvironment(int index, const char *code) {
+  lua_getglobal(L, "update_env");
+  lua_pushinteger(L, index);
+  lua_pushstring(L, code);
+  if (lua_pcall(L, 2, 0, 0) != LUA_OK) {
+    printf("Error: %s\n", lua_tostring(L, -1));
+    lua_close(L);
+    return 1;
+  }
+  return 0;
+}
+
+float luaRunMain(int index) {
+  lua_getglobal(L, "env_main");
+  lua_pushinteger(L, index);
+  if (lua_pcall(L, 1, 1, 0) != LUA_OK) {
+    printf("Error: %s\n", lua_tostring(L, -1));
+    lua_close(L);
+    return 0;
+  }
+  return lua_tonumber(L, -1);
+}
+
+int luaTest() {
+  /**
+  -- a = 10
+-- function main()
+--     a = a + 1
+--     return a
+-- end
+  **/
+  // add above code as update environment
+  const char *code =
+      "a = 10\n"
+      "function main()\n"
+      "    a = a + 1\n"
+      "    return a\n"
+      "end";
+  luaUpdateEnvironment(0, code);
+
+  // add another environment
+  const char *code2 =
+      "a = 20\n"
+      "function main()\n"
+      "    a = a + random_number()\n"
+      "    return a\n"
+      "end";
+  luaUpdateEnvironment(1, code2);
+  printf("Result: %f\n", luaRunMain(0));
+  printf("Result: %f\n", luaRunMain(0));
+  printf("Result: %f\n", luaRunMain(0));
+  printf("Result: %f\n", luaRunMain(0));
+  printf("Result: %f\n", luaRunMain(1));
+  printf("Result: %f\n", luaRunMain(1));
+  printf("Result: %f\n", luaRunMain(1));
+  printf("Result: %f\n", luaRunMain(1));
+  const char *code3 =
+      "a = 1\n"
+      "function main()\n"
+      "    a = a + 2\n"
+      "    return a\n"
+      "end";
+  luaUpdateEnvironment(1, code3);
+  printf("Result: %f\n", luaRunMain(1));
+  printf("Result: %f\n", luaRunMain(1));
+  printf("Result: %f\n", luaRunMain(1));
+  printf("Result: %f\n", luaRunMain(1));
+
   return 0;
 }
--- lib/script.lua
+++ lib/script.lua
@@ -1,3 +1,33 @@
 function random_number()
-    return math.random(1, 1100)
+    return math.random(100, 150)
 end
+
+function new_env(code)
+    local env = {}
+    setmetatable(env, {
+        __index = function(_, key)
+            return _G[key] or 0
+        end,
+        __newindex = function(_, key, value)
+            rawset(env, key, value)
+        end
+    })
+    local chunk = load(code, "chunk", "t", env)
+    chunk()
+    return env
+end
+
+envs = {}
+for i = 1, 8 do
+    table.insert(envs, new_env(""))
+end
+
+function update_env(i, code)
+    envs[i] = new_env(code)
+end
+
+function env_main(i)
+    return envs[i].main()
+end
+
+math.randomseed(os.time())
