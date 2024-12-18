8b0899fad5433661ae0690057de620b3899667ee
Zack Scholl <zack.scholl@gmail.com>
2024-12-18 04:40:00 -0800

    ok

--- lib/mcp4725.h
+++ lib/mcp4725.h
@@ -11,12 +11,15 @@
 typedef struct MCP4725 {
   i2c_inst_t *i2c;
   float voltage;
+  float voltage_reference;
   uint8_t address;
 } MCP4725;
 
-void MCP4725_init(MCP4728 *self, i2c_inst_t *i2c, bool address_bit) {
+void MCP4725_init(MCP4728 *self, i2c_inst_t *i2c, bool address_bit,
+                  float external_voltage) {
   self->i2c = i2c;
   self->address = MCP4728_ADDRESS + address_bit;
+  self->voltage_reference = external_voltage;
 }
 
 #endif
